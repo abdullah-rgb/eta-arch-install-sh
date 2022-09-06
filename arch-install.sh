1782755d2215cf4f737a1f70d966d087b26471ff
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-09-06 23:56:33 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -12,7 +12,7 @@ clear
 # disk configuration
 lsblk
 echo ' '
-read -p 'Disk secin (Example: vda, sda, nvme0n1): ' disk_selection
+read -p 'Select Disk (Example: vda, sda, nvme0n1): ' disk_selection
 cfdisk /dev/$disk_selection
 
 clear
@@ -20,13 +20,9 @@ lsblk
 echo ' '
 
 # format disk label
-read -p 'Diskinizin Root bolumunu secin (Example: vda1, sda1, nvme0n1p1): ' root_partition
+read -p 'Select Root partition (Example: vda1, sda1, nvme0n1p1): ' root_partition
 mkfs.ext4 /dev/$root_partition
 mount /dev/$root_partition /mnt
-
-read -p 'Diskinizi Boot bolumunu secin (Example: vda1, sda1, nvme0n1p1): ' boot_partition
-mkfs.fat -F32 /dev/$boot_partition
-mkdir -p /mnt/boot/efi
 
 clear
 
@@ -35,7 +31,6 @@ pacstrap /mnt base base-devel linux linux-headers linux-firmware git nano bc
 
 # create fstab file
 genfstab -U /mnt > /mnt/etc/fstab
-mount /dev/$boot_partition /mnt/boot/efi
 
 cp $(pwd)/chroot.sh /mnt
 arch-chroot /mnt /bin/bash
